28126d20ca7518a60b0f23b708e93405f7c6b692
Samuele Panzeri <samuele.panzeri@gmail.com>
2020-12-03 11:52:39 +0100

    Added vscode workspace and modified build.zig to work with vscode

--- 2020/build.zig
+++ 2020/build.zig
@@ -1,11 +1,11 @@
-const std = @import("std");
-const Builder = std.build.Builder;
+const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) !void {
     const mode = b.standardReleaseOptions();
 
-    const day01 = b.addExecutable("day01", "src/day_01.zig");
+    const day01 = b.addExecutable("advent", "src/day_01.zig");
     day01.setBuildMode(mode);
+    day01.install();
     const run01_cmd = day01.run();
     const run01_step = b.step("run01", "Run day 01");
     run01_step.dependOn(&run01_cmd.step);
