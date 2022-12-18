ea18448ee07634a54bea4cb09bc42a3067121e45
Samuele Panzeri <samuele.panzeri@gmail.com>
2022-12-18 02:19:15 +0100

    2022 - Day 16 solution 1 and 2, but I had to find inspiration online to do it

--- 2022/src/day16.cpp
+++ 2022/src/day16.cpp
@@ -22,7 +22,7 @@ int main()
 	}
 }
 
-constexpr int make_corridor_index(std::string_view name)
+constexpr int make_index_from_label(std::string_view name)
 {
 	return ((int)name[0] | ((int)name[1] << 8));
 }
@@ -42,14 +42,14 @@ std::string_view check_and_skip_prefix(std::string_view src, std::string_view pr
 std::string_view parse_corridor_index(std::string_view src, std::vector<int>& corridors)
 {
 	assert(src.size() >= 2);
-	corridors.push_back(make_corridor_index(src));
+	corridors.push_back(make_index_from_label(src));
 	return {begin(src) + 2, end(src)};
 }
 
 Valve parse_valve(std::string_view src) {
 	Valve res = {};
 	src = check_and_skip_prefix(src, "Valve ");
-	res.index = make_corridor_index(src);
+	res.index = make_index_from_label(src);
 	src = {begin(src) + 2, end(src)};
 	src = check_and_skip_prefix(src, " has flow rate=");
 	src = parse_value(src, res.flow_rate);
@@ -70,6 +70,78 @@ Valve parse_valve(std::string_view src) {
 	return res;
 }
 
+struct Bitset
+{
+	uint64_t data = 0;
+	void set(int index) { data |= 1ull << index; }
+	bool test(int index) const { return (data & (1ull << index)) != 0; }
+	void reset(int index) { data &= ~(1ull << index); }
+	void flip(int index) { data ^= 1ull << index; }
+};
+
+int compute_open_flow(std::vector<Valve>& valves, Bitset open)
+{
+	int accum = 0;
+	for (int vi = 0; vi < (int)valves.size(); vi++)
+	{
+		if (open.test(vi))
+			accum += valves[vi].flow_rate;
+	}
+	return accum;
+}
+
+int dfs(
+	std::vector<Valve>& valves, int index, const std::vector<std::vector<int>>& distances,
+	Bitset open, int time_left, int released_pressure)
+{
+	int open_flow = compute_open_flow(valves, open);
+	int max = released_pressure + open_flow * time_left;
+
+	for (int vi = 0; vi < (int)valves.size(); vi++) {
+		if (vi == index)
+			continue;
+		if (valves[vi].flow_rate == 0)
+			continue;
+		if (open.test(vi))
+			continue;
+
+		// Time to move there and open the valve
+		int delta_time = distances[index][vi] + 1;
+		if (time_left - delta_time < 2)
+			continue;
+
+		int new_total = released_pressure + open_flow * delta_time;
+		open.set(vi);
+		int new_max = dfs(valves, vi, distances, open, time_left - delta_time, new_total);
+		max = std::max(max, new_max);
+		open.reset(vi);
+	}
+
+	return max;
+}
+
+int solve(std::vector<Valve>& valves, int first_valve)
+{
+	std::vector<std::vector<int>> distances(valves.size(), std::vector<int>(valves.size(), (int)valves.size() + 1));
+
+	for (size_t i = 0; i < valves.size(); i++) {
+		distances[i][i] = 0;
+		for (int adj : valves[i].corridors)
+			distances[i][adj] = 1;
+	}
+
+	// Run the Floyd-Warshall algorithm to update the distance matrix.
+	for (size_t k = 0; k < valves.size(); k++) {
+		for (size_t i = 0; i < valves.size(); i++) {
+			for (size_t j = 0; j < valves.size(); j++) {
+				distances[i][j] = std::min(distances[i][j], distances[i][k] + distances[k][j]);
+			}
+		}
+	}
+
+	return dfs(valves, first_valve, distances, Bitset{}, 30, 0);
+}
+
 void solution1()
 {
 	std::string_view filename{"input/day16.txt"};
@@ -80,6 +152,7 @@ void solution1()
 	std::vector<Valve> valves;
 	std::unordered_map<int, int> remap;
 	std::string line;
+
 	while (input) {
 		std::getline(input, line);
 		if (line.empty())
@@ -89,13 +162,95 @@ void solution1()
 		valves.push_back(valve);
 	}
 
-	for (auto& valve : valves) {
+	int first_valve = 0;
+	int start_index = make_index_from_label("AA");
+	for (int vi = 0; vi < (int)valves.size(); vi++)
+	{
+		auto &valve = valves[vi];
+		if (valve.index == start_index)
+			first_valve = vi;
 		for (auto& corridor : valve.corridors) {
 			corridor = remap[corridor];
 		}
 	}
 
-	println("Solution1: {}", 0);
+	println("Solution1: {}", solve(valves, first_valve));
+}
+
+bool should_open(std::vector<Valve>& valves, Bitset open, int current, int valve)
+{
+	if (valve == current)
+		return false;
+	if (open.test(valve))
+		return false;
+	return valves[valve].flow_rate > 0;
+}
+
+struct InitalState {
+	int index;
+	int time_left;
+};
+
+int dfs2(
+	std::vector<Valve>& valves, bool consider_other, InitalState& init, int index, const std::vector<std::vector<int>>& distances,
+	Bitset open, Bitset ignore, int time_left, int released_pressure)
+{
+	int open_flow = compute_open_flow(valves, open);
+	int max = released_pressure + open_flow * time_left;
+
+	if (consider_other)
+	{
+		int other_max = dfs2(valves, false, init, init.index, distances, Bitset{}, open, init.time_left, 0);
+		max += other_max;
+	}
+
+	for (int vi = 0; vi < (int)valves.size(); vi++) {
+		if (vi == index)
+			continue;
+		if (valves[vi].flow_rate == 0)
+			continue;
+		if (open.test(vi))
+			continue;
+		if (!consider_other && ignore.test(vi))
+			continue;
+
+		// Time to move there and open the valve
+		int delta_time = distances[index][vi] + 1;
+		if (time_left - delta_time < 2)
+			continue;
+
+		int new_total = released_pressure + open_flow * delta_time;
+		open.set(vi);
+		int new_max = dfs2(valves, consider_other, init, vi, distances, open, ignore, time_left - delta_time, new_total);
+		max = std::max(max, new_max);
+		open.reset(vi);
+	}
+
+	return max;
+}
+
+int solve2(std::vector<Valve>& valves, int first_valve)
+{
+	std::vector<std::vector<int>> distances(valves.size(), std::vector<int>(valves.size(), (int)valves.size() + 1));
+
+	for (size_t i = 0; i < valves.size(); i++) {
+		distances[i][i] = 0;
+		for (int adj : valves[i].corridors)
+			distances[i][adj] = 1;
+	}
+
+	// Run the Floyd-Warshall algorithm to update the distance matrix.
+	for (size_t k = 0; k < valves.size(); k++) {
+		for (size_t i = 0; i < valves.size(); i++) {
+			for (size_t j = 0; j < valves.size(); j++) {
+				distances[i][j] = std::min(distances[i][j], distances[i][k] + distances[k][j]);
+			}
+		}
+	}
+
+	InitalState init{.index = first_valve, .time_left = 26};
+
+	return dfs2(valves, true, init, init.index, distances, Bitset{}, Bitset{}, init.time_left, 0);
 }
 
 void solution2()
@@ -105,5 +260,30 @@ void solution2()
 	if (!input.is_open())
 		fatal_error("Missing file: {}", filename);
 
-	println("Solution2: {}", 0);
+	std::vector<Valve> valves;
+	std::unordered_map<int, int> remap;
+	std::string line;
+
+	while (input) {
+		std::getline(input, line);
+		if (line.empty())
+			continue;
+		auto valve = parse_valve(line);
+		remap[valve.index] = (int)valves.size();
+		valves.push_back(valve);
+	}
+
+	int first_valve = 0;
+	int start_index = make_index_from_label("AA");
+	for (int vi = 0; vi < (int)valves.size(); vi++)
+	{
+		auto &valve = valves[vi];
+		if (valve.index == start_index)
+			first_valve = vi;
+		for (auto& corridor : valve.corridors) {
+			corridor = remap[corridor];
+		}
+	}
+
+	println("Solution1: {}", solve2(valves, first_valve));
 }
