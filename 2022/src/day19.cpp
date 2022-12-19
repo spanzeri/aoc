32976da14fcfd33eeaf30a976d37ebe4e6e0c151
Samuele Panzeri <samuele.panzeri@gmail.com>
2022-12-19 23:34:06 +0100

    Debuggin day 19, switch PC again

--- 2022/src/day19.cpp
+++ 2022/src/day19.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <vector>
 #include <array>
+#include <unordered_set>
 
 void solution1();
 void solution2();
@@ -43,6 +44,22 @@ struct State {
 	MatArray robots;
 	MatArray resources;
 };
+
+bool operator==(const State& a, const State& b) {
+	return a.resources == b.resources && a.robots == b.robots;
+}
+
+namespace std {
+template <>
+struct hash<State> {
+	size_t operator()(const State& s) const {
+		size_t k = 0;
+		for (int i = 0; i < Material::COUNT; i++)
+			k ^= hash<int>()(s.robots[i]) | hash<int>()(s.resources[i]) << 32;
+		return k;
+	}
+};
+}
 
 std::string_view consume_prefix(std::string_view src, std::string_view prefix)
 {
@@ -124,6 +141,72 @@ State build_robot(const State& prev, const Blueprint& bp, Material mat)
 
 int solve_1bp(State initial, const Blueprint& bp, int time)
 {
+	MatArray maxes = {};
+	for (auto& cost : bp.costs) {
+		for (int i = 0; i < Material::COUNT; i++)
+			maxes[i] = std::max(maxes[i], cost[i]);
+	}
+
+	MatArray latest = {};
+	latest[Material::COUNT - 1] = time - 1;
+	for (int mat = Material::COUNT - 2; mat >= 0; --mat) {
+		latest[mat] = latest[mat + 1] - maxes[mat];
+	}
+
+	std::unordered_set<State> states[2];
+	states[0].insert(initial);
+
+	for (int ct = 0; ct < time; ct++) {
+		if (ct == 21) {
+			int x = 0;
+			x = x;
+		}
+		println("At time {} num states {}", ct, states[0].size());
+
+		for (const auto& state : states[0]) {
+			State current = make_new_state(state);
+
+			bool useless = false;
+			for (int mat = 1; mat < Material::COUNT; mat++) {
+				if (current.robots[mat] == 0 && ct > latest[mat]) {
+					//useless = true;
+					break;
+				}
+			}
+			if (useless)
+				continue;
+			for (int mat = 0; mat < Material::COUNT - 1; mat++) {
+				if (current.resources[mat] > maxes[mat]) {
+					useless = true;
+					break;
+				}
+			}
+			if (useless)
+				continue;
+
+			for (int mat = Material::COUNT - 1; mat >= 0; mat--)
+			{
+				if (current.robots[mat] >= maxes[mat])
+					continue;
+				if (!can_build_robot(current, bp, (Material)mat))
+					continue;
+				states[1].insert(build_robot(current, bp, (Material)mat));
+			}
+
+			states[1].insert(current);
+		}
+
+		states[0] = {};
+		swap(states[0], states[1]);
+	}
+
+	int max = 0;
+	for (const auto& state : states[0]) {
+		if (state.resources[Geode] > max)
+			max = state.resources[Geode];
+	}
+
+	return max;
 }
 
 int solve(std::vector<Blueprint>& bps, int time) {
