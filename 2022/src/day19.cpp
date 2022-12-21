dc7acf8d1ddd4f1f717f2647265f6fd3cbc04d6f
Samuele Panzeri <samuele.panzeri@gmail.com>
2022-12-21 22:51:19 +0100

    2022 - Day 21: solution 1 and 2

--- 2022/src/day19.cpp
+++ 2022/src/day19.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <array>
 #include <unordered_set>
+#include <unordered_map>
+#include <queue>
 
 void solution1();
 void solution2();
@@ -31,22 +33,23 @@ enum Material {
 	COUNT
 };
 
-using MatArray = std::array<int, Material::COUNT>;
-using CostArray = std::array<MatArray, Material::COUNT>;
-
 struct Blueprint
 {
-	int index;
-	CostArray costs;
+	int index = 0;
+	int costs[Material::COUNT][2] = {};
 };
 
 struct State {
-	MatArray robots;
-	MatArray resources;
+	int robots[Material::COUNT] = {};
+	int resources[Material::COUNT] = {};
+	int time = 0;
 };
 
 bool operator==(const State& a, const State& b) {
-	return a.resources == b.resources && a.robots == b.robots;
+	for (int i = 0; i < Material::COUNT; i++)
+		if (a.robots[i] != b.robots[i] && a.resources[i] != b.resources[i])
+			return false;
+	return a.time == b.time;
 }
 
 namespace std {
@@ -55,7 +58,8 @@ struct hash<State> {
 	size_t operator()(const State& s) const {
 		size_t k = 0;
 		for (int i = 0; i < Material::COUNT; i++)
-			k ^= hash<int>()(s.robots[i]) | hash<int>()(s.resources[i]) << 32;
+			k ^= hash<int>()(s.robots[i]) ^ hash<int>()(s.resources[i]);
+		k ^= hash<int>()(s.time);
 		return k;
 	}
 };
@@ -79,21 +83,26 @@ std::string_view parse_mat(std::string_view src, Material& out_mat)
 	unreacheable();
 }
 
-std::string_view parse_costs(std::string_view src, MatArray& costs) {
+std::string_view parse_costs(std::string_view src, Blueprint& bp, Material mat) {
 	for (;;) {
 		src = trim(src);
 		if (src.empty())
 			return src;
-		int amount;
-		Material mat;
-		src = parse_value(src, amount);
+		Material base;
+		src = parse_value(src, bp.costs[mat][0]);
 		src = trim(src);
-		src = parse_mat(src, mat);
-		costs[mat] = amount;
-		if (src.starts_with(" and "))
+		src = parse_mat(src, base);
+		assert(base == Ore);
+		if (mat >= Obsidian) {
+			assert(src.starts_with(" and "));
 			src = {begin(src) + sizeof(" and ") - 1, end(src)};
-		else if (src.starts_with("."))
-			return {begin(src) + 1, end(src)};
+			src = parse_value(src, bp.costs[mat][1]);
+			src = trim(src);
+			src = parse_mat(src, base);
+			assert(base == mat - 1);
+		}
+		assert(src.starts_with("."));
+		return {begin(src) + 1, end(src)};
 	}
 }
 
@@ -110,113 +119,92 @@ Blueprint parse_blueprint(std::string_view src) {
 		Material produced_mat;
 		src = parse_mat(src, produced_mat);
 		src = consume_prefix(src, " robot costs ");
-		src = parse_costs(src, bp.costs[(size_t)produced_mat]);
+		src = parse_costs(src, bp, produced_mat);
 	}
 }
 
-State make_new_state(const State& prev)
+int solve_1bp(const Blueprint& bp, int time)
 {
-	State next = prev;
+	int max_geodes = 0;
+
+	int max_robots[Material::COUNT];
+	max_robots[Geode] = INT_MAX;
 	for (int i = 0; i < Material::COUNT; i++)
-		next.resources[i] += next.robots[i];
-	return next;
-}
+		max_robots[Ore] = std::max(max_robots[Ore], bp.costs[i][0]);
+	for (int i = 1; i < Material::Geode; i++)
+		max_robots[i] = bp.costs[i + 1][1];
 
-bool can_build_robot(const State& state, const Blueprint& bp, Material mat)
-{
-	for (int i = 0; i < Material::COUNT; i++)
-		if (state.resources[i] < bp.costs[mat][i])
-			return false;
-	return true;
-}
-
-State build_robot(const State& prev, const Blueprint& bp, Material mat)
-{
-	State next = prev;
-	for (int i = 0; i < Material::COUNT; i++)
-		next.resources[i] -= bp.costs[mat][i];
-	next.robots[mat] += 1;
-	return next;
-}
-
-int solve_1bp(State initial, const Blueprint& bp, int time)
-{
-	MatArray maxes = {};
-	for (auto& cost : bp.costs) {
-		for (int i = 0; i < Material::COUNT; i++)
-			maxes[i] = std::max(maxes[i], cost[i]);
-	}
-
-	MatArray latest = {};
-	latest[Material::COUNT - 1] = time - 1;
-	for (int mat = Material::COUNT - 2; mat >= 0; --mat) {
-		latest[mat] = latest[mat + 1] - maxes[mat];
-	}
-
-	std::unordered_set<State> states[2];
-	states[0].insert(initial);
-
-	for (int ct = 0; ct < time; ct++) {
-		if (ct == 21) {
-			int x = 0;
-			x = x;
-		}
-		println("At time {} num states {}", ct, states[0].size());
-
-		for (const auto& state : states[0]) {
-			State current = make_new_state(state);
-
-			bool useless = false;
-			for (int mat = 1; mat < Material::COUNT; mat++) {
-				if (current.robots[mat] == 0 && ct > latest[mat]) {
-					//useless = true;
-					break;
-				}
-			}
-			if (useless)
-				continue;
-			for (int mat = 0; mat < Material::COUNT - 1; mat++) {
-				if (current.resources[mat] > maxes[mat]) {
-					useless = true;
-					break;
-				}
-			}
-			if (useless)
-				continue;
-
-			for (int mat = Material::COUNT - 1; mat >= 0; mat--)
-			{
-				if (current.robots[mat] >= maxes[mat])
-					continue;
-				if (!can_build_robot(current, bp, (Material)mat))
-					continue;
-				states[1].insert(build_robot(current, bp, (Material)mat));
-			}
-
-			states[1].insert(current);
-		}
-
-		states[0] = {};
-		swap(states[0], states[1]);
-	}
-
-	int max = 0;
-	for (const auto& state : states[0]) {
-		if (state.resources[Geode] > max)
-			max = state.resources[Geode];
-	}
-
-	return max;
-}
-
-int solve(std::vector<Blueprint>& bps, int time) {
 	State init_state = {};
 	init_state.robots[Ore] = 1;
 
+	std::vector<State> states;
+	states.push_back(init_state);
+
+	std::unordered_set<State> state_cache;
+
+	auto mine = [](State &state) {
+		for (int i = 0; i < Material::COUNT; ++i)
+			state.resources[i] += state.robots[i];
+		state.time += 1;
+	};
+
+	while (!states.empty())
+	{
+		State prev = states.back();
+		states.pop_back();
+
+		// Advance the state
+		State curr = prev;
+		mine(curr);
+
+		if (curr.time == time) {
+			if (curr.resources[Geode] > max_geodes)
+				max_geodes = curr.resources[Geode];
+			continue;
+		}
+
+		// Push a state where we do nothing
+		if (!state_cache.contains(curr)) {
+			states.push_back(curr);
+			state_cache.insert(curr);
+		}
+
+		// Build a robot. Skip if we have too many for a type or if we could have
+		// built it in the previous state and decided not to (as it wouldn't make sense)
+		for (int i = 0; i < Material::COUNT; i++)
+		{
+			if (curr.time >= time - 2)
+				continue;
+			if (curr.robots[i] == max_robots[i])
+				continue;
+			if (curr.resources[Ore] < bp.costs[i][0] || (i > Clay && curr.resources[i - 1] < bp.costs[i][1]))
+				continue;
+			if (prev.resources[Ore] >= bp.costs[i][0] && (i <= Clay || prev.resources[i - 1] >= bp.costs[i][1]))
+				continue;
+
+			// Build the robot
+			State curr_with_robot = curr;
+			curr_with_robot.resources[Ore] -= bp.costs[i][0];
+			if (i > Clay)
+				curr_with_robot.resources[i - 1] -= bp.costs[i][1];
+			curr_with_robot.robots[i] += 1;
+			mine(curr_with_robot);
+
+			if (!state_cache.contains(curr_with_robot)) {
+				states.push_back(curr_with_robot);
+				state_cache.insert(curr_with_robot);
+			}
+		}
+	}
+
+	return max_geodes;
+}
+
+int solve(std::vector<Blueprint>& bps, int time) {
 	int result = 0;
 	for (auto& bp : bps)
 	{
-		int max = solve_1bp(init_state, bp, time);
+		int max = solve_1bp(bp, time);
 		println("Max geode for blueprint: {} = {}, Quality score: {}", bp.index, max, bp.index * max);
 		result += bp.index * max;
 	}
---
+++ 2022/src/day21.cpp
@@ -0,0 +1,271 @@
+#include "common.h"
+
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <array>
+#include <unordered_set>
+#include <unordered_map>
+
+void solution1();
+void solution2();
+
+int main()
+{
+	{
+		SimpleTimer timer{"solution1"};
+		solution1();
+	}
+	{
+		SimpleTimer timer{"solution2"};
+		solution2();
+	}
+}
+
+struct MonkeyMath {
+	enum Op {
+		Add,
+		Mul,
+		Sub,
+		Div
+	} op;
+	std::string lhs;
+	std::string rhs;
+};
+
+struct Monkey
+{
+	enum Job {
+		Yell,
+		Math
+	} job;
+	union
+	{
+		int64_t value;
+		int64_t index;
+	};
+};
+
+std::string_view parse_name(std::string_view src, std::string& dest)
+{
+	src = trim(src);
+	auto b = begin(src);
+	auto e = b;
+	while (e != end(src) && *e >= 'a' && *e <= 'z')
+		++e;
+	dest = std::string(b, e);
+	return {e, end(src)};
+}
+
+std::string parse_monkey(std::string_view src, Monkey& monkey, std::vector<MonkeyMath>& math_ops)
+{
+	std::string name;
+	src = parse_name(src, name);
+	assert(src.starts_with(": "));
+	src = {begin(src) + 2, end(src)};
+	assert(!src.empty());
+	if (src[0] >= 'a' && src[0] <= 'z') {
+		MonkeyMath math;
+		src = parse_name(src, math.lhs);
+		if (src.starts_with(" + "))
+			math.op = MonkeyMath::Add;
+		else if (src.starts_with(" * "))
+			math.op = MonkeyMath::Mul;
+		else if (src.starts_with(" - "))
+			math.op = MonkeyMath::Sub;
+		else if (src.starts_with(" / "))
+			math.op = MonkeyMath::Div;
+		else
+			unreacheable();
+		src = {begin(src) + 3, end(src)};
+		parse_name(src, math.rhs);
+		monkey.job = Monkey::Math;
+		monkey.index = (int)math_ops.size();
+		math_ops.push_back(std::move(math));
+	}
+	else {
+		monkey.job = Monkey::Yell;
+		parse_value(src, monkey.value);
+	}
+	return name;
+}
+
+int64_t solve_monkey(std::string_view name, std::unordered_map<std::string, Monkey>& monkeys, std::vector<MonkeyMath>& math_ops)
+{
+	auto &monkey = monkeys[std::string(name)];
+	if (monkey.job == Monkey::Yell)
+		return monkey.value;
+
+	auto &math = math_ops[monkey.index];
+	int64_t lhs = solve_monkey(math.lhs, monkeys, math_ops);
+	int64_t rhs = solve_monkey(math.rhs, monkeys, math_ops);
+
+	int64_t res = 0;
+	switch (math.op) {
+	case MonkeyMath::Add: res = lhs + rhs; break;
+	case MonkeyMath::Mul: res = lhs * rhs; break;
+	case MonkeyMath::Sub: res = lhs - rhs; break;
+	case MonkeyMath::Div: res = lhs / rhs; break;
+	}
+
+	// Caching
+	monkey.job = Monkey::Yell;
+	monkey.value = res;
+
+	return res;
+}
+
+void solution1()
+{
+	std::string_view filename{"input/day21.txt"};
+	std::ifstream input{filename.data(), std::ifstream::in};
+	if (!input.is_open())
+		fatal_error("Missing file: {}", filename);
+
+	std::string line;
+	std::vector<MonkeyMath> math_ops;
+	std::unordered_map<std::string, Monkey> monkeys;
+
+	while (input) {
+		std::getline(input, line);
+		if (line.empty())
+			continue;
+
+		Monkey monkey;
+		std::string name = parse_monkey(line, monkey, math_ops);
+		monkeys[name] = monkey;
+	}
+
+	println("Solution1: {}", solve_monkey("root", monkeys, math_ops));
+}
+
+struct Variable
+{
+	MonkeyMath::Op op;
+	bool lhs_variable = false;
+	int64_t constant = 0;
+};
+
+bool solve2_eq(
+	std::string_view name, std::unordered_map<std::string, Monkey>& monkeys, std::vector<MonkeyMath>& math_ops,
+	std::vector<Variable>& variables, int64_t& result)
+{
+	if (name == "humn")
+		return false;
+
+	auto &monkey = monkeys[std::string(name)];
+	if (monkey.job == Monkey::Yell) {
+		result = monkey.value;
+		return true;
+	}
+
+	auto &math = math_ops[monkey.index];
+
+	int64_t lhs, rhs;
+	bool solved_lhs = solve2_eq(math.lhs, monkeys, math_ops, variables, lhs);
+	bool solved_rhs = solve2_eq(math.rhs, monkeys, math_ops, variables, rhs);
+
+	if (solved_lhs && solved_rhs) {
+		switch (math.op) {
+		case MonkeyMath::Add: result = lhs + rhs; break;
+		case MonkeyMath::Mul: result = lhs * rhs; break;
+		case MonkeyMath::Sub: result = lhs - rhs; break;
+		case MonkeyMath::Div: result = lhs / rhs; break;
+		}
+
+		// Caching
+		monkey.job = Monkey::Yell;
+		monkey.value = result;
+
+		return true;
+	}
+
+	if (!solved_lhs) {
+		Variable var;
+		var.op = math.op;
+		var.lhs_variable = true;
+		var.constant = rhs;
+		variables.push_back(var);
+	}
+	else {
+		Variable var;
+		var.op = math.op;
+		var.lhs_variable = false;
+		var.constant = lhs;
+		variables.push_back(var);
+	}
+
+	return false;
+}
+
+int64_t solve2(std::string_view name, std::unordered_map<std::string, Monkey>& monkeys, std::vector<MonkeyMath>& math_ops)
+{
+	auto &root_monkey = monkeys[std::string(name)];
+	auto &eq = math_ops[root_monkey.index];
+
+	// Try and solve backward (with variable)
+	std::vector<Variable> variables;
+	int64_t lhs, rhs;
+	bool humn_left  = !solve2_eq(eq.lhs, monkeys, math_ops, variables, lhs);
+	bool humn_right = !solve2_eq(eq.rhs, monkeys, math_ops, variables, rhs);
+	assert(humn_left != humn_right);
+
+	int64_t matchv = humn_left ? rhs : lhs;
+
+	// Solve variable forward
+	for (int i = (int)variables.size() - 1; i >= 0; --i) {
+		const auto &var = variables[i];
+		switch(var.op)
+		{
+		case MonkeyMath::Add: {
+			matchv = matchv - var.constant;
+			break;
+		}
+
+		case MonkeyMath::Sub: {
+			if (var.lhs_variable)
+				matchv = matchv + var.constant;
+			else
+				matchv = var.constant - matchv;
+			break;
+		}
+
+		case MonkeyMath::Mul: {
+			matchv = matchv / var.constant;
+			break;
+		}
+
+		case MonkeyMath::Div: {
+			matchv = matchv * var.constant;
+			break;
+		}
+		}
+	}
+
+	return matchv;
+}
+
+void solution2()
+{
+	std::string_view filename{"input/day21.txt"};
+	std::ifstream input{filename.data(), std::ifstream::in};
+	if (!input.is_open())
+		fatal_error("Missing file: {}", filename);
+
+	std::string line;
+	std::vector<MonkeyMath> math_ops;
+	std::unordered_map<std::string, Monkey> monkeys;
+
+	while (input) {
+		std::getline(input, line);
+		if (line.empty())
+			continue;
+
+		Monkey monkey;
+		std::string name = parse_monkey(line, monkey, math_ops);
+		monkeys[name] = monkey;
+	}
+
+	println("Solution2: {}", solve2("root", monkeys, math_ops));
+}
